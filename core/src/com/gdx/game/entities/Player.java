2b89c0bd1d1ef2c7ae37b103e2d05d03b2acbc36
giovannibianco21 <58288920+giovannibianco21@users.noreply.github.com>
2019-12-03 16:38:23 +0100

    story 44
    As a player, i want to be able to move my character

--- core/src/com/gdx/game/entities/Player.java
+++ core/src/com/gdx/game/entities/Player.java
@@ -8,46 +8,59 @@ package com.gdx.game.entities;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
-import sun.net.www.http.KeepAliveCache;
+import com.gdx.game.GdxGame;
+import com.gdx.game.screens.GameScreen;
 
 /**
  *
  * @author Giovanni
  */
 public class Player extends Entity {
-    
+
+    TextureAtlas atlas;
+    GameScreen screen;
+    GdxGame game;
+
     public Player(World world, float width, float height, Vector2 position) {
         super(world, width, height, position);
         initPhysics();
+        initGraphics();
+
     }
-    
+
     @Override
     protected void initPhysics() {
         BodyDef bdDef = new BodyDef();
         bdDef.type = BodyDef.BodyType.DynamicBody;
         bdDef.position.set(300, 400);
         body = world.createBody(bdDef);
-        
-        CircleShape shape = new CircleShape();
-        shape.setRadius(60f);
-        
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(width / 2, height / 2);
+
         FixtureDef fixDef = new FixtureDef();
         fixDef.shape = shape;
         fixDef.density = 0.5f;
-        
+
         body.createFixture(fixDef);
         shape.dispose();
     }
-    
+
     @Override
     protected void initGraphics() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        atlas = new TextureAtlas(Gdx.files.internal("texture/player/test_pack.atlas"));
+        textureRegion = atlas.findRegion("down");
     }
 
     //ci servono i metodi ACT e DRAW
@@ -72,7 +85,24 @@ public class Player extends Entity {
         if (!body.getLinearVelocity().equals(velocity)) {
             body.setLinearVelocity(velocity);
         }
-        
+
+        if (Gdx.input.isKeyJustPressed(Keys.W)) {
+            textureRegion = atlas.findRegion("up");
+        } else if (Gdx.input.isKeyJustPressed(Keys.S)) {
+            textureRegion = atlas.findRegion("down");
+        } else if (Gdx.input.isKeyJustPressed(Keys.A)) {
+            textureRegion = atlas.findRegion("left");
+        } else if (Gdx.input.isKeyJustPressed(Keys.D)) {
+            textureRegion = atlas.findRegion("right");
+        }
+
     }
-    
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) { //Draw dice al batch cosa deve disegnare. Lo stage ogni volta che fai stage.draw chiama tutti i draw degli actors passandogli il batch in modo che possono contribire al batch e disegna tutto insieme
+
+        batch.draw(textureRegion, body.getPosition().x - width / 2, body.getPosition().y - height / 2, width, height);
+
+    }
+
 }
--- core/src/com/gdx/game/screens/GameScreen.java
+++ core/src/com/gdx/game/screens/GameScreen.java
@@ -20,29 +20,29 @@ import com.gdx.game.entities.Player;
  * @author Armando
  */
 public class GameScreen implements Screen {
-    
+
     private Stage stage;
     private Game game;
     private World world;
     private Player player;
     private Box2DDebugRenderer debugRenderer;
     private InputEventQueue inputEQ;
-    
+
     public GameScreen(Game aGame) {
         this.game = aGame;
         stage = new Stage();
         world = new World(Vector2.Zero, true);
         debugRenderer = new Box2DDebugRenderer();
         inputEQ = new InputEventQueue(stage);
-        Player player = new Player(world, 0, 0, new Vector2(300, 400));
+        Player player = new Player(world, 50, 50, new Vector2(300, 400));
         stage.addActor(player);
     }
-    
+
     @Override
     public void show() {
         Gdx.input.setInputProcessor(inputEQ);
     }
-    
+
     @Override
     public void render(float f) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
@@ -52,24 +52,24 @@ public class GameScreen implements Screen {
         debugRenderer.render(world, stage.getCamera().combined);
         world.step(1 / 60f, 6, 2);
     }
-    
+
     @Override
     public void dispose() {
         stage.dispose();
     }
-    
+
     @Override
     public void resize(int i, int i1) {
     }
-    
+
     @Override
     public void pause() {
     }
-    
+
     @Override
     public void resume() {
     }
-    
+
     @Override
     public void hide() {
     }
