0a9891deea15087daaf078f25c4b3d1e9eaa51c7
KillEmFlacko <KillEmFlacko@users.noreply.github.com>
2019-12-06 16:00:59 +0100

    map

--- core/src/com/gdx/game/screens/GameScreen.java
+++ core/src/com/gdx/game/screens/GameScreen.java
@@ -1,18 +1,15 @@
 package com.gdx.game.screens;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.ContactImpulse;
-import com.badlogic.gdx.physics.box2d.ContactListener;
-import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.gdx.game.GdxGame;
@@ -33,6 +30,8 @@ public class GameScreen implements Screen {
     private World world;
     private Player player;
     private Box2DDebugRenderer debugRenderer;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer mapRenderer;
 
     static final int WORLD_WIDTH = 100;
     static final int WORLD_HEIGHT = 100;
@@ -42,12 +41,13 @@ public class GameScreen implements Screen {
         stage = new Stage(aGame.vp);
         world = new World(Vector2.Zero, true);
         world.setContactListener(new ContactMultiplexer(new ImprovedContactListener()));
+        map = new TmxMapLoader().load("mappaTest/mappaTest.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map, 25f / Gdx.graphics.getWidth());
         debugRenderer = new Box2DDebugRenderer();
         player = new Player("uajono", 100, world, 16 / GdxGame.SCALE, 28 / GdxGame.SCALE, new Vector2(10, 10));
         stage.addActor(player);
 
         MovementSetFactory mvsf = MovementSetFactory.instanceOf();
-//        Vector2 v = new Vector2(Gdx.graphics.getWidth() * 2 / 3, Gdx.graphics.getHeight() * 2 / 3);
         Vector2 v = player.getPosition().add(5, 5);
         //v is the player position to substitute when merge is complete
         DemoBoss db = new DemoBoss("Nameless King", 30, this.world, 20 / GdxGame.SCALE, 20 / GdxGame.SCALE, v, mvsf.build("Fast", "Square", false, v, 3));
@@ -72,10 +72,13 @@ public class GameScreen implements Screen {
     public void render(float f) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        world.step(1 / 60f, 6, 2);
         stage.act();
+        mapRenderer.setView((OrthographicCamera) stage.getCamera());
+        stage.getCamera().update();
+        mapRenderer.render();
         stage.draw();
         debugRenderer.render(world, stage.getCamera().combined);
-        world.step(1 / 60f, 6, 2);
     }
 
     @Override
