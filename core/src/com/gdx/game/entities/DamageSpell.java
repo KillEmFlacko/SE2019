7067957de4ea47840399ab2c41a04ad06f2440c1
nataliafinelli <58288905+nataliafinelli@users.noreply.github.com>
2019-12-09 17:39:33 +0100

    classes boostspell, defencespell, damagespell

---
+++ core/src/com/gdx/game/entities/DamageSpell.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gdx.game.entities;
+
+/**
+ *
+ * @author natal
+ */
+public class DamageSpell implements Skill {
+
+    @Override
+    public void getBaseCooldown() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCooldown() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getCooldown() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
