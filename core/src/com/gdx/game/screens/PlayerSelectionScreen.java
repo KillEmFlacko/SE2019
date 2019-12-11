d0d6530257618f159f69851875afea49b7a8f6d8
FerdyGuar97 <ferdy97@hotmail.it>
2019-12-11 12:32:54 +0100

    PlayerSelectionScreen update
    A cooler player selection screen.

--- core/src/com/gdx/game/screens/PlayerSelectionScreen.java
+++ core/src/com/gdx/game/screens/PlayerSelectionScreen.java
@@ -4,21 +4,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.gdx.game.GdxGame;
+import com.gdx.game.entities.Player;
 import com.gdx.game.entities.classes.EasternWizard;
 import com.gdx.game.entities.classes.NorthernWizard;
 import com.gdx.game.entities.classes.SouthernWizard;
 import com.gdx.game.entities.classes.WesternWizard;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -35,10 +44,16 @@ public class PlayerSelectionScreen implements Screen {
     private final int BUTTON_SPACE = 5;
     private float colWidth;
     private float rowHeight;
+    
+    private Player player, player2, player3, player4;
+    private final World world;
 
     public PlayerSelectionScreen(GdxGame aGame) {
         this.game = aGame;
         this.stage = new Stage(aGame.vp);
+
+        world = new World(Vector2.Zero, true);
+        
         initUI();
     }
 
@@ -52,7 +67,7 @@ public class PlayerSelectionScreen implements Screen {
         BitmapFont font = generator.generateFont(parameters);
         generator.dispose();
 
-        Label.LabelStyle lblStyle = new Label.LabelStyle();
+        final Label.LabelStyle lblStyle = new Label.LabelStyle();
         lblStyle.font = font;
 
         labelTitle = new Label("Choose Your Wizard", lblStyle);
@@ -60,7 +75,9 @@ public class PlayerSelectionScreen implements Screen {
         labelTitle.setAlignment(Align.center);
         labelTitle.setPosition(0, Gdx.graphics.getHeight() - labelTitle.getHeight()*2);
         stage.addActor(labelTitle);
-
+        
+        /*
+        
         TextButton btnButton = new TextButton("Northern Wizard", game.skin, "default");
         btnButton.setSize(Gdx.graphics.getWidth() / 5, Gdx.graphics.getHeight() / 15);
         btnButton.setPosition(Gdx.graphics.getWidth() / 2 - btnButton.getWidth() / 2, Gdx.graphics.getHeight() / 2 - btnButton.getHeight() / 2 - padding);
@@ -112,6 +129,8 @@ public class PlayerSelectionScreen implements Screen {
             }
         });
         stage.addActor(btnButton4);
+
+        */
         
         colWidth = Gdx.graphics.getWidth() / 5f;
         rowHeight = Gdx.graphics.getHeight() / 15f;
@@ -127,6 +146,91 @@ public class PlayerSelectionScreen implements Screen {
             }
         });
         stage.addActor(btnButton5);
+        
+        //image
+        
+        float playerWorldWidth = 16 / GdxGame.SCALE * 20;
+        float playerWorldHeight = 28 / GdxGame.SCALE * 20;
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        //(14.623319,19.27667)  (15, 15 * (h / w))
+        
+        game.vp.setWorldSize(800, 800 * (h / w)); // 30 * aspectRatio
+
+        
+        player = new Player("uajono", world, playerWorldWidth, playerWorldHeight, new Vector2(400,500), new NorthernWizard());
+        // Constructs a new OrthographicCamera, using the given viewport width and height
+        // Height is multiplied by aspect ratio.
+        player.setBounds(player.getPosition().x - player.getWorldWidth() / 2, player.getPosition().y - player.getWorldHeight() / 2, player.getWorldWidth(), player.getWorldHeight());
+        player.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                PlayerSelectionScreen.this.dispose();
+                game.setScreen(new GameScreen(game, new NorthernWizard()));
+                return true;
+            }
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+            }
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+            }
+        });
+        stage.addActor(player);
+        
+        player2 = new Player("uajono", world, playerWorldWidth, playerWorldHeight, new Vector2(400,200), new SouthernWizard());
+        player2.setBounds(player2.getPosition().x - player2.getWorldWidth() / 2, player2.getPosition().y - player2.getWorldHeight() / 2, player.getWorldWidth(), player.getWorldHeight());
+        player2.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                PlayerSelectionScreen.this.dispose();
+                game.setScreen(new GameScreen(game, new SouthernWizard()));
+                return true;
+            }
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+            }
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+            }
+        });
+        stage.addActor(player2);
+        
+        player3 = new Player("uajono", world, playerWorldWidth, playerWorldHeight, new Vector2(600,350), new EasternWizard());
+        player3.setBounds(player3.getPosition().x - player3.getWorldWidth() / 2, player3.getPosition().y - player3.getWorldHeight() / 2, player.getWorldWidth(), player.getWorldHeight());
+        player3.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                PlayerSelectionScreen.this.dispose();
+                game.setScreen(new GameScreen(game, new EasternWizard()));
+                return true;
+            }
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+            }
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+            }
+        });
+        stage.addActor(player3);
+        
+        player4 = new Player("uajono", world, playerWorldWidth, playerWorldHeight, new Vector2(200,350), new WesternWizard());
+        player4.setBounds(player4.getPosition().x - player4.getWorldWidth() / 2, player4.getPosition().y - player4.getWorldHeight() / 2, player.getWorldWidth(), player.getWorldHeight());
+        player4.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                PlayerSelectionScreen.this.dispose();
+                game.setScreen(new GameScreen(game, new WesternWizard()));
+                return true;
+            }
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+            }
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+            }
+        });
+        stage.addActor(player4);
     }
 
     private void initPhy() {
