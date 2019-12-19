453bda959e79841d33a04760601ac6835c0cf6e2
DavideDellaMonica <52746946+DavideDellaMonica@users.noreply.github.com>
2019-12-19 16:51:41 +0100

    Creata interfaccia EntityDef

---
+++ core/src/com/gdx/game/entities/EntityDef.java
@@ -0,0 +1,35 @@
+package com.gdx.game.entities;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.utils.ObjectMap;
+import java.util.Map;
+
+/**
+ *
+ * @author david
+ */
+public interface EntityDef {
+
+    public BodyDef getBodyDef();
+
+    public ObjectMap<String, FixtureDef> getFixtureDefs();
+
+    public Map<String, Animation> getAnimations();
+    
+    public float getWidth();
+    
+    public void setWidth(float width);
+    
+    public float getHeight();
+    
+    public void setWidtHeight(float width);
+    
+    
+    // !! modificatore d'accesso protected non permesso
+    public float getCustomScale();
+    
+    // !! modificatore d'accesso protected non permesso
+    public void setCustomScale();
+}
